8c844ee48d4f569fbdbe4e3ed40a62a7137e52ce
Josh Raiborde <joshsrai@yahoo.com>
2020-08-31 18:46:26 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -30,7 +30,7 @@ class Student
     VALUES (?, ?)
     SQL
     DB[:conn].execute(sql, self.name, self.grade)
-    @id = DB[:conn].execute("SELECT last_insert_rowed() FROM students").flatten[0]
+    @id = DB[:conn].execute("SELECT last_insert_rowid() FROM students").flatten[0]
   end
   
 
